ab26e5039382c4aa5813ccae03e33df200eb4a52
altusea <zt.song@outlook.com>
2025-04-13 10:57:51 +0800

    stage

--- src/main/java/moe/nova/playground/TimeTest3.java
+++ src/main/java/moe/nova/playground/TimeTest3.java
@@ -4,6 +4,7 @@ import org.dromara.hutool.core.date.DateFormatPool;
 import org.dromara.hutool.core.date.TimeUtil;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class TimeTest3 {
 
@@ -15,5 +16,7 @@ public class TimeTest3 {
         date1 = LocalDate.parse("20191212", DateFormatPool.PURE_DATE_FORMATTER);
         date2 = LocalDate.parse("20221212", DateFormatPool.PURE_DATE_FORMATTER);
         System.out.println(TimeUtil.betweenPeriod(date1, date2).getYears());
+
+        System.out.println(LocalDateTime.now().minusMonths(1L).format(DateFormatPool.NORM_DATETIME_FORMATTER));
     }
 }
