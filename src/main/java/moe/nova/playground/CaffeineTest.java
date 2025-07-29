9863a855814ea08b915dd2f7fccce5120f570df1
altusea <zt.song@outlook.com>
2025-07-29 21:36:30 +0800

    build: set version to 2025.8.0

--- src/main/java/moe/nova/playground/CaffeineTest.java
+++ src/main/java/moe/nova/playground/CaffeineTest.java
@@ -3,20 +3,21 @@ package moe.nova.playground;
 import com.github.benmanes.caffeine.cache.AsyncLoadingCache;
 import com.github.benmanes.caffeine.cache.Caffeine;
 import com.github.benmanes.caffeine.cache.LoadingCache;
+import org.jetbrains.annotations.NotNull;
 
 import java.time.Duration;
 
 public class CaffeineTest {
 
     static void main(String[] args) {
-        LoadingCache<String, Integer> cache = Caffeine.newBuilder()
+        LoadingCache<@NotNull String, Integer> cache = Caffeine.newBuilder()
                 .expireAfterWrite(Duration.ofMinutes(30L))
                 .build(String::length);
         cache.put("test", 4);
         IO.println(cache.get("test"));
         IO.println(cache.get("test1"));
 
-        AsyncLoadingCache<String, Integer> cache2 = Caffeine.newBuilder()
+        AsyncLoadingCache<@NotNull String, Integer> cache2 = Caffeine.newBuilder()
                 .expireAfterWrite(Duration.ofMinutes(30L))
                 .buildAsync(k -> {
                     Thread.sleep(1000L);
--- src/main/java/moe/nova/playground/EclipseCollectionsTest.java
+++ src/main/java/moe/nova/playground/EclipseCollectionsTest.java
@@ -1,6 +1,9 @@
 package moe.nova.playground;
 
 import org.eclipse.collections.api.factory.Lists;
+import org.eclipse.collections.api.multimap.list.MutableListMultimap;
+import org.eclipse.collections.impl.multimap.list.FastListMultimap;
+import org.eclipse.collections.impl.set.mutable.UnifiedSet;
 
 public class EclipseCollectionsTest {
 
@@ -8,5 +11,18 @@ public class EclipseCollectionsTest {
         var a = Lists.immutable.of(0L, 1L, 2L);
         var b = a.zipWithIndex();
         System.out.println(b);
+
+        var s = UnifiedSet.<String>newSet();
+        s.add("apple");
+        s.add("banana");
+        s.add("orange");
+        s.add("apple");
+        System.out.println(s);
+
+        MutableListMultimap<String, String> m = new FastListMultimap<>();
+        m.put("a", "1");
+        m.put("a", "2");
+        m.put("b", "3");
+        System.out.println(m);
     }
 }
