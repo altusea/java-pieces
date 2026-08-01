11e2c8da3fea3ed8a3f8d84e2f80b11bdfa24d35
altusea <zt.song@outlook.com>
2026-08-01 23:02:50 +0800

    test

---
+++ src/main/java/moe/nova/playground/serde/FastjsonTest.java
@@ -0,0 +1,18 @@
+package moe.nova.playground.serde;
+
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONWriter;
+import com.google.common.collect.ImmutableMap;
+
+public class FastjsonTest {
+
+    static void main() {
+        ImmutableMap<String, Long> map = ImmutableMap.of(
+                "a", 1L,
+                "aa", 10L,
+                "aaa", 100L,
+                "aaaa", 1000L
+        );
+        IO.println(JSON.toJSONString(map, JSONWriter.Feature.WriteClassName));
+    }
+}
