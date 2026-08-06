1c139818124b70568025d9c4c5a0f7cb2bf1c93e
altusea <zt.song@outlook.com>
2026-08-06 19:03:56 +0800

    test

--- src/main/java/moe/nova/playground/serde/FastjsonTest.java
+++ src/main/java/moe/nova/playground/serde/FastjsonTest.java
@@ -8,10 +8,10 @@ public class FastjsonTest {
 
     static void main() {
         ImmutableMap<String, Long> map = ImmutableMap.of(
-                "a", 1L,
-                "aa", 10L,
-                "aaa", 100L,
-                "aaaa", 1000L
+                "aaaa", 1L,
+                "aaa", 10L,
+                "aa", 100L,
+                "a", 1000L
         );
         var a = JSON.toJSONString(map, JSONWriter.Feature.WriteClassName);
         System.out.println(a);
