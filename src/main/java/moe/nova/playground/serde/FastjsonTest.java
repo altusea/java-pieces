61ff73a594304da013c1a19cfb2bae7258b79a3d
altusea <zt.song@outlook.com>
2026-08-19 19:35:57 +0800

    添加 HashMap 示例代码

--- src/main/java/moe/nova/playground/serde/FastjsonTest.java
+++ src/main/java/moe/nova/playground/serde/FastjsonTest.java
@@ -4,9 +4,17 @@ import com.alibaba.fastjson2.JSON;
 import com.alibaba.fastjson2.JSONWriter;
 import com.google.common.collect.ImmutableMap;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class FastjsonTest {
 
     static void main() {
+        Map<String, String> hashMap = new HashMap<>();
+        hashMap.put("a", "1");
+        hashMap.put("b", "2");
+        System.out.println(hashMap);
+
         ImmutableMap<String, Long> map = ImmutableMap.of(
                 "aaaa", 1L,
                 "aaa", 10L,
